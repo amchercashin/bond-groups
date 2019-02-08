c30afe733b183c27242cbfdf9c00a1ceee86aa5d
amchercashin <amchercashin@gmail.com>
2019-02-08 16:23:27 +0300

    counter implement

---
+++ track.js
@@ -0,0 +1,20 @@
+var currentEncodedURL = encodeURIComponent(window.location.href)
+var API_ENDPOINT = "https://0biunsjmwk.execute-api.us-east-1.amazonaws.com/dev/nandolytics/"
+var getEndpoint = API_ENDPOINT + currentEncodedURL
+var recordEndpoint = API_ENDPOINT + "record"
+
+// Record a visit to this site
+fetch(recordEndpoint, {
+  method: 'post',
+  mode: 'no-cors',
+  body: JSON.stringify({"siteUrl":currentEncodedURL})
+}).then(r => r.json())
+  .then(data => console.log(data))
+  .catch(e => console.log("The POST request for Nandolytics failed"))
+
+// Console log current site visits
+fetch(getEndpoint).then(r => r.json())
+  .then(data => {
+    console.log(data['siteHits'])
+  })
+  .catch(e => console.log("The GET request for Nandolytics failed"))
