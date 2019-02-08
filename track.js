d83c5df3f47275963f5b53398257872c732f2035
amchercashin <amchercashin@gmail.com>
2019-02-08 17:50:11 +0300

    diable GET counter - not working due to CORS

--- track.js
+++ track.js
@@ -7,14 +7,15 @@ var recordEndpoint = API_ENDPOINT + "record"
 fetch(recordEndpoint, {
   method: 'post',
   crossDomain: true,
+  "Content-Type": "application/json",
   body: JSON.stringify({"siteUrl":currentEncodedURL})
 }).then(r => r.json())
   .then(data => console.log(data))
   .catch(e => console.log("The POST request for Nandolytics failed"))
 
 // Console log current site visits
-fetch(getEndpoint,{crossDomain: true}).then(r => r.json())
-  .then(data => {
-    console.log(data['siteHits'])
-  })
-  .catch(e => console.log("The GET request for Nandolytics failed"))
+// fetch(getEndpoint).then(r => r.json())
+//   .then(data => {
+//     console.log(data['siteHits'])
+//   })
+//   .catch(e => console.log("The GET request for Nandolytics failed"))
